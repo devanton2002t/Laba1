411d8c70fa87611c1dee30282a320919869b508a
ZANYRI <zanyri880@gmail.com>
2023-05-16 18:39:29 +0300

    Initial commit

---
+++ Laba1/FL.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ConsoleApp.PostgreSQL
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Profile> Profiles{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=3183");
+
+    }
+    [Table("profiles", Schema = "public")]
+    public class Profile
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("age")]
+        public int Age { get; set; }
+      
+    }
+ }
---
+++ Laba1/Program.cs
@@ -0,0 +1,110 @@
+﻿using ConsoleApp.PostgreSQL;
+using Laba1;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте!");
+            while (true)
+            {
+            Console.WriteLine("Выберите функцию:");
+            Console.WriteLine("1-Создать");
+            Console.WriteLine("2-Удалить");
+            Console.WriteLine("3-Обновить");
+            Console.WriteLine("4-Проверить");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введите имя:");
+                    string n = Console.ReadLine();
+                    Console.WriteLine("Введите возраст:");
+                    int a = Int32.Parse(Console.ReadLine());
+                    using (BloggingContext context = new BloggingContext())
+                    {
+                        context.Profiles.Add(new Profile() { Name = n, Age = a });
+                        context.SaveChanges();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Введите ID профиля:");
+                    int i = Int32.Parse(Console.ReadLine());
+                    using (BloggingContext context = new BloggingContext())
+                    {
+                        var userToDelete = context.Profiles.Find(i);
+                        if (userToDelete != null)
+                        {
+                            Console.WriteLine($"Вы действительно хотите удалить пользователя {userToDelete.Name}? (Y/N)");
+                            var key = Console.ReadKey().Key;
+                            if (key == ConsoleKey.Y)
+                            {
+                                context.Profiles.Remove(userToDelete);
+                                    context.SaveChanges();
+                                Console.WriteLine( $"\nПользователь {userToDelete.Name} удален из базы данных.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Удаление пользователя {userToDelete.Name} отменено.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Пользователь с идентификатором {i} не найден в базе данных.");
+                        }
+                    }
+                    break;
+                    case 3:
+                    Console.WriteLine("Введите ID профиля:");
+                    int i1 = Int32.Parse(Console.ReadLine());
+                    using (BloggingContext context = new BloggingContext())
+                    {
+                            var userToUpdate = context.Profiles.Find(i1);
+                            if (userToUpdate != null)
+                            {
+                                Console.WriteLine($"Введите новое имя для пользователя {userToUpdate.Name}:");
+                                userToUpdate.Name = Console.ReadLine();
+                                Console.WriteLine($"Введите новое имя для пользователя {userToUpdate.Age}:");
+                                userToUpdate.Age = Int32.Parse(Console.ReadLine());
+                                context.SaveChanges();
+                                Console.WriteLine($"Данные пользователя {userToUpdate.Name} успешно обновлены.");
+                                Console.WriteLine($"Данные пользователя {userToUpdate.Age} успешно обновлены.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Пользователь с идентификатором {i1} не найден в базе данных.");
+                            }
+                        
+                        break;
+                    }
+                    case 4:
+                    using (BloggingContext context = new BloggingContext())
+                    {
+                        var users = context.Profiles.ToList();
+                        Console.WriteLine("Список пользователей:");
+                        foreach (var user in users)
+                        {
+                            Console.WriteLine($"{user.Id}: {user.Name} {user.Age}");
+                        }
+                    }
+                    break;
+            }
+
+                Console.WriteLine("Хотите запустить программу заново? (Y/N)");
+                var input = Console.ReadLine();
+                if (input.ToLower() != "y")
+                {
+                    break; 
+                }
+            }
+        }
+    }
+}
